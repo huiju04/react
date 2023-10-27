e8c3017c8ca5e439b44fee51b7642404765d26f0
huiju04 <henjou04@naver.com>
2023-10-27 17:50:26 +0900

    홈페이지

--- react_23/src/GlobalStyled.js
+++ react_23/src/GlobalStyled.js
@@ -21,7 +21,12 @@ export const GlobalStyled = createGlobalStyle`
         box-sizing: border-box;
     }
     body{
-        /* font-family: 'Noto Sans KR', sans-serif; */
+        font-family: 'Noto Sans KR', sans-serif;
+    }
+
+    a{
+      text-decoration: none;
+      color: #1d1d1d;
     }
 `;
 // => 전체 영역에서 사용할 수 있는 스타일 처리법
